bf2841b0fffa424c1fe15faf3bac02bf0d6a242b
Justin Venezuela <jven@jvenezue.la>
2021-11-26 10:39:53 -0500

    Removes bounce stuff.

--- game.js
+++ game.js
@@ -8,12 +8,7 @@ const resetGame = (pics) => {
   const randomPicUrl = pics[Math.floor(Math.random() * pics.length)];
 
   const round = new Round([
-    new Pic(
-        randomPicUrl,
-        /* size= */ [500, 300],
-        /* position= */ [100, 100],
-        /* direction= */ 'NW',
-        /* speed= */ 0)
+    new Pic(randomPicUrl, /* position= */ [100, 100])
   ], /* winningIndex= */ 0);
   round.renderIntoStage(getStage());
 
@@ -68,17 +63,13 @@ class Round {
 }
 
 class Pic {
-  constructor(imageUrl, size, position, direction, speed) {
-    this.size_ = size.concat();
+  constructor(imageUrl, position) {
     this.position_ = position.concat();
-    this.direction_ = direction;
-    this.speed_ = speed;
-    this.lastUpdateTime_ = 0;
 
     this.imageEl_ = document.createElement('img');
     this.imageEl_.src = imageUrl;
-    this.imageEl_.style.width = this.size_[0] + 'px';
-    this.imageEl_.style.height = this.size_[1] + 'px';
+    this.imageEl_.style.left = `${position[0]}px`;
+    this.imageEl_.style.top = `${position[1]}px`;
     this.imageEl_.classList.add('pic');
   }
 
@@ -92,99 +83,13 @@ class Pic {
   }
 
   markEndOfRound() {
-    this.speed_ = 0;
   }
 
   update() {
-    const nowTime = Date.now();
-    const elapsedTimeMs = nowTime - this.lastUpdateTime_;
-    this.lastUpdateTime_ = nowTime;
-
-    this.maybeBounce_(elapsedTimeMs);
-    this.position_ = this.calculateNextPosition_(this.direction_, elapsedTimeMs);
-    this.imageEl_.style.left = this.position_[0] + 'px';
-    this.imageEl_.style.top = this.position_[1] + 'px';
   }
 
   dispose() {
     this.imageEl_.remove();
-  }
-
-  maybeBounce_(elapsedTimeMs) {
-    const TEST_DISTANCE = 10;
-    switch (this.direction_) {
-      case 'NW':
-        if (!this.isAddInBounds_(-TEST_DISTANCE, 0) && this.isAddInBounds_(0, -TEST_DISTANCE)) {
-          this.direction_ = 'NE';
-        } else if (!this.isAddInBounds_(0, -TEST_DISTANCE) && this.isAddInBounds_(-TEST_DISTANCE, 0)) {
-          this.direction_ = 'SW';
-        } else if (!this.isAddInBounds_(-TEST_DISTANCE, -TEST_DISTANCE)) {
-          this.direction_ = 'SE';
-        }
-        break;
-      case 'NE':
-        if (!this.isAddInBounds_(TEST_DISTANCE, 0) && this.isAddInBounds_(0, -TEST_DISTANCE)) {
-          this.direction_ = 'NW';
-        } else if (!this.isAddInBounds_(0, -TEST_DISTANCE) && this.isAddInBounds_(TEST_DISTANCE, 0)) {
-          this.direction_ = 'SE';
-        } else if (!this.isAddInBounds_(TEST_DISTANCE, -TEST_DISTANCE)) {
-          this.direction_ = 'SW';
-        }
-        break;
-      case 'SE':
-        if (!this.isAddInBounds_(TEST_DISTANCE, 0) && this.isAddInBounds_(0, TEST_DISTANCE)) {
-          this.direction_ = 'SW';
-        } else if (!this.isAddInBounds_(0, TEST_DISTANCE) && this.isAddInBounds_(TEST_DISTANCE, 0)) {
-          this.direction_ = 'NE';
-        } else if (!this.isAddInBounds_(TEST_DISTANCE, TEST_DISTANCE)) {
-          this.direction_ = 'NW';
-        }
-        break;
-      case 'SW':
-        if (!this.isAddInBounds_(-TEST_DISTANCE, 0) && this.isAddInBounds_(0, TEST_DISTANCE)) {
-          this.direction_ = 'SE';
-        } else if (!this.isAddInBounds_(0, TEST_DISTANCE) && this.isAddInBounds_(-TEST_DISTANCE, 0)) {
-          this.direction_ = 'NW';
-        } else if (!this.isAddInBounds_(-TEST_DISTANCE, TEST_DISTANCE)) {
-          this.direction_ = 'NE';
-        }
-        break;
-    }
-  }
-
-  isAddInBounds_(dLeft, dTop) {
-    const addPos = [this.position_[0] + dLeft, this.position_[1] + dTop];
-    return this.isPositionInBounds_(addPos);
-  }
-
-  calculateNextPosition_(direction, elapsedTimeMs) {
-    let dLeft = 0;
-    let dTop = 0;
-    switch (direction) {
-      case 'NW':
-        dLeft = -1;
-        dTop = -1;
-        break;
-      case 'NE':
-        dLeft = 1;
-        dTop = -1;
-        break;
-      case 'SE':
-        dLeft = 1;
-        dTop = 1;
-        break;
-      case 'SW':
-        dLeft = -1;
-        dTop = 1;
-        break;
-    }
-
-    return [this.position_[0] + dLeft * elapsedTimeMs * this.speed_, this.position_[1] + dTop * elapsedTimeMs * this.speed_];
-  }
-
-  isPositionInBounds_(position) {
-    return position[0] >= 0 && (position[0] + this.size_[0]) < window.innerWidth
-        && position[1] >= 0 && (position[1] + this.size_[1]) < window.innerHeight;
   }
 }
 
